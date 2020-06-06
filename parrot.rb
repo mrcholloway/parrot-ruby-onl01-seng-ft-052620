77afe12d093288e0f05a3ae7790c79e03d91f986
Christopher Holloway <mrcholloway@yahoo.com>
2020-06-06 20:35:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,4 +2,11 @@ def parrot(parrot_sound = "Squawk!")
   puts parrot_sound
   return parrot_sound
   end
+<<<<<<< HEAD
   
+=======
+  
+parrot("Polly wants a cracker")
+parrot("E = MC Squared")
+parrot
+>>>>>>> 8fca6ad2d2eeec4ea57c8a1c88666757a452fdf2
